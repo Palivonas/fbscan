d185622a81f214713454b12224a672c28866cc7f
Mr.Field <cosimocampo@gmail.com>
2015-03-07 16:08:44 +0000

    Almost done

--- static/quandary.js
+++ static/quandary.js
@@ -1,58 +1,85 @@
-// None of this is used anymore
-//
+"use strict";
 
-function fetchGroupList() {
+function getFormData() {
+    document.cookie="token="+$("input:text").val();
+    document.cookie="group_id="+$("input:radio:checked").val();
+    
+    var args = $("#group-list-form").serialize();
+    displayTab(args, "general");
+}
+
+
+function getCookieData() {
+    var cookie_strings = document.cookie.split("; "); 
+    var cookie = {};
+    for (var i = 0; i < cookie_strings.length; i++) {
+        cookie[ cookie_strings[i].split("=")[0] ] = cookie_strings[i].split("=")[1];
+    }
+    return cookie;
+}
+
+
+function displayTab(args, tab, no_fade) {
+    $.get("/"+ tab +"?" + args, function(response) {
+        var page = $("#container");
+        
+        if (!no_fade) {
+            page.fadeOut(function() {
+                page.empty();
+                page.append(response);
+                
+                page.fadeIn();
+                $(".tabs").fadeIn();
+            });
+        } else {
+            page.empty();
+            page.append(response);
+            page.show();
+            $(".tabs").show();
+        }
+    });
+}
+
+
+function changeTab(tab) {
+    $("ul", "#options").hide();
+    var cookie = getCookieData();
+    displayTab($.param(cookie), tab);
+}
+
+
+function listGroups(token, no_fade) {
     var list = $("#group-list-form");
-    FB.api("me/groups?fields=icon,name", function(response) {
+    
+    // Gets the list of groups from Facebook
+    $.getJSON("https://graph.facebook.com/v2.2/me/groups?fields=name,icon&access_token=" + token, function(response) {
         for (var i = 0; i < response.data.length; i++) {
             var group = response.data[i];
-            var option = "<div class='group'><img src='" + group.icon + "'><input type='radio' name='group_id' value='" + group.id + "'><p>" + group.name + "</p></div>";     
+            var option = "<div class='group'><img src='" + group.icon + "'><input type='radio' name='group_id' value='" + group.id + "'><p>" + group.name + "</p></div>";
             list.append(option);
         }
-        list.append("<input type='submit' value='submit' style='float:right; margin-top:10px'>");
-    });
-}
-  
-  
-function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        fetchGroupList();
-        $(".fb-login-button").hide();
-        $("#group-list-form").append("<input type='text' name='access_token' style='display:none' value='" + response.authResponse.accessToken + "'>"); //Kludge
-        $("#group-list").show();
-    } else {
-        alert("Something went wrong. Please try again");
-        $("#group-list").hide();
-        $(".fb-login-button").show();
-    }
-}
-
-  
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+        
+        list.append("Access token: <input type='text' style='margin: 10px 10px 0px 0px' name='access_token' value='" + token + "'>");
+        list.append("<input type='button' id='go' onclick='getFormData()' value='Go!' style='float:right; clear:both; margin-top:10px'>");
+        
+        if (no_fade) {
+            $("#group-list").show();
+        } else {
+            $("#token-dialog").fadeOut(function() {
+                $("#token-dialog").remove();
+                $("#group-list").fadeIn();
+            });
+        }
+        
     });
 }
 
-  
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '626019544198223',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v2.2'
-    });
-  
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-};
 
-  
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+function resetGroup() {
+    document.cookie = "group_id=";
+}
+
+
+function resetToken() {
+    document.cookie = "token=";
+}
